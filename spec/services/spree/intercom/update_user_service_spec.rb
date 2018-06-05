49fe1e907d77a3b9a4e75714807753c792fbe026
Shivam Jain <shivam@vinsol.com>
2018-06-05 14:22:17 +0530

    update specs

--- spec/services/spree/intercom/update_user_service_spec.rb
+++ spec/services/spree/intercom/update_user_service_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe Spree::Intercom::UpdateUserService, type: :service do
   Spree::Config.intercom_application_id = ''
 
   let!(:user) { create(:user) }
+  let!(:user_data) { Spree::Intercom::UserSerializer.new(user).serializable_hash }
   let!(:user_service) { Spree::Intercom::UpdateUserService.new(user.id) }
   let!(:intercom) { Intercom::Client.new(token: Spree::Config.intercom_access_token) }
-  let!(:user_data) { Spree::Intercom::UserSerializer.new(user).serializable_hash }
 
   describe '#initialize' do
     it 'is expected to set @user' do
@@ -41,12 +41,41 @@ RSpec.describe Spree::Intercom::UpdateUserService, type: :service do
   end
 
   describe '#update' do
+    let!(:response) { 'response' }
+    let!(:event_service) { Spree::Intercom::Events::User::UpdateService.new(user.id) }
+
     before do
       allow(user_service).to receive(:send_request).and_return(true)
+      user_service.instance_variable_set(:@response, response)
+      allow(response).to receive(:success?).and_return(true)
     end
 
     it 'is expected to call send_request' do
       expect(user_service).to receive(:send_request).and_return(true)
+    end
+
+    context 'response is success' do
+      before do
+        allow(Spree::Intercom::Events::User::UpdateService).to receive(:new).with(user.id).and_return(event_service)
+      end
+
+      it 'is expected to call service' do
+        expect(Spree::Intercom::Events::User::UpdateService).to receive(:new).with(user.id).and_return(event_service)
+      end
+
+      it 'is expected to call register' do
+        expect(event_service).to receive(:register)
+      end
+    end
+
+    context 'response is failure' do
+      before do
+        allow(response).to receive(:success?).and_return(false)
+      end
+
+      it 'is not expected to call service' do
+        expect(Spree::Intercom::Events::User::CreateService).not_to receive(:new).with(user.id)
+      end
     end
 
     after { user_service.update }
